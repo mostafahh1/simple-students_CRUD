2254b8d3ab662c0fabf1e3e7b74b1483cd57271b
mostafahh1 <mmoossttaaffaa6666663@gmail.com>
2026-05-15 09:14:25 -0700

    Update Main.java

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,7 +1,15 @@
 package application;
 import ui.MainConsoleUi;
-
+/**
+ * Entry point of the Student CRUD Application.
+ * It starts the console user interface.
+ */
 public class Main {
+     /**
+     * Main method that launches the application.
+     *
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         MainConsoleUi mainConsoleUi = new MainConsoleUi();
     }
