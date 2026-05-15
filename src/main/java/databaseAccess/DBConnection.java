67ecdd017cd3e1255ea59c25bcccda99a7fb431d
MostafaHesham321 <20240603@stud.fci-cu.edu.eg>
2026-05-15 07:29:52 -0700

    getConnection to app.db

--- src/main/java/databaseAccess/DBConnection.java
+++ src/main/java/databaseAccess/DBConnection.java
@@ -1,5 +1,54 @@
 package databaseAccess;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
 
+    private static Connection conn;
+    private static final String URL = "jdbc:sqlite:app.db";
+
+
+    private static void createDB() {
+
+        try {
+            conn = DriverManager.getConnection(URL);
+            createTableStudent();
+        } catch (Exception e) {
+            System.out.println("Error connecting to database: " + e.getMessage());
+
+        }
+    }
+
+
+    private static void createTableStudent() {
+
+        String sql = """
+
+                CREATE TABLE IF NOT EXISTS students (
+
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    gpa REAL
+
+                );
+
+                """;
+
+        try {
+            conn.createStatement().execute(sql);
+        } catch (Exception e) {
+            System.out.println("Error creating table: " + e.getMessage());
+
+        }
+    }
+
+    public static Connection getConnection() {
+
+        if (conn == null) {
+            createDB();
+        }
+
+        return conn;
+    }
 }
